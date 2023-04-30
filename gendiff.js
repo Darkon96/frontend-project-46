4209316cbca59c30b6541addbd26101fd6cbd8f9
Darkon96 <steponframe2@gmail.com>
2023-04-30 23:10:35 +0300

    step1

--- gendiff.js
+++ gendiff.js
@@ -1,20 +1,10 @@
 #!/usr/bin/env node
 /* eslint-disable import/extensions */
 
-const { Command } = require('commander');
-const program = new Command();
+import { program } from 'commander';
 
 program
-  .name('string-util')
-  .description('CLI to some JavaScript string utilities')
-  .version('0.8.0');
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0');
 
-program.command('split')
-  .description('Split a string into substrings and display as an array')
-  .argument('<string>', 'string to split')
-  .option('--first', 'display just the first substring')
-  .option('-s, --separator <char>', 'separator character', ',')
-  .action((str, options) => {
-    const limit = options.first ? 1 : undefined;
-    console.log(str.split(options.separator, limit));
-  })
+program.parse();
