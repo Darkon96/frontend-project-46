20e7e302359003e59e961ec2e5da196bfd45dd8b
Darkon96 <steponframe2@gmail.com>
2023-12-04 15:59:39 +0300

    15:59_04.12.2023

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -8,11 +8,11 @@ export default (data, format, replacer) => {
   switch (format) {
     case 'stylish':
       return makeStylish(data, replacer);
-      case 'plain':
-        return makePlain(data);
-      case 'json':
-        return makeJson(data, null, replacer);
-      default:
-        throw new Error(`Invalid file format type: '.${format}'! Try supported file formats.`);
-    }
-  };
+    case 'plain':
+      return makePlain(data);
+    case 'json':
+      return makeJson(data, null, replacer);
+    default:
+      throw new Error(`Invalid file format type: '.${format}'! Try supported file formats.`);
+  }
+};
